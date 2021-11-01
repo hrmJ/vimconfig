e6524c1d607cf6ee96b396236adbed6d9fd6c291
Juho Härme <juho.harme@knowit.fi>
2021-11-01 12:01:11 +0200

    add cmp-buffer as plugin

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-
 require('plugins')
 require('_lsp-config')
 require('cmp-config')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -42,6 +42,7 @@ require('packer').startup(function()
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-buffer'
   use 'saadparwaiz1/cmp_luasnip'
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
   use 'christianchiarulli/nvcode-color-schemes.vim'
