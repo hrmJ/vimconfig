849da54f2d0808e83970cda9c8126b933b4d9211
Juho Härme <juho.harme@knowit.fi>
2021-11-01 11:42:53 +0200

    add sources to  /

--- lua/cmp-config.lua
+++ lua/cmp-config.lua
@@ -32,31 +32,14 @@ cmp.setup {
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
-    },
-    ['<Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end,
+    }
   },
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
     { name = "path" },
   },
+
 
   formatting = {
     format = lspkind.cmp_format {
@@ -82,3 +65,20 @@ cmp.setup {
 
 
 }
+
+-- Use buffer source for `/`.
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+
+-- Use cmdline & path source for ':'.
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
