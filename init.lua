dacc3a30346f1bfdb584215d9d78d3f92a9158de
Juho Härme <juho.harme@gmail.com>
2021-11-05 21:39:15 +0200

    use null-ts + ts-lsp-utils together with eslint-lsp

--- init.lua
+++ init.lua
@@ -26,6 +26,8 @@ require('packer').startup(function()
 
   use 'neovim/nvim-lspconfig'
   use 'williamboman/nvim-lsp-installer'
+  use 'jose-elias-alvarez/null-ls.nvim'
+  use 'jose-elias-alvarez/nvim-lsp-ts-utils'
 
   use {'tomasiser/vim-code-dark', config ='vim.cmd[[colorscheme codedark]]'}
   use {'ray-x/navigator.lua', requires = {'ray-x/guihua.lua', run = 'cd lua/fzy && make'}}
@@ -45,11 +47,51 @@ require('packer').startup(function()
 
 end)
 
-
+require("null-ls").config {}
+require("lspconfig")["null-ls"].setup {}
 require'navigator'.setup({
    lsp = {
-     format_on_save = true,
-     diagnostic_virtual_text = true  
+     format_on_save = false,
+     diagnostic_virtual_text = false,
+     tsserver = {
+         -- filetypes = {"typescript", "typescriptreact", "javascript", "javascriptreact"},
+         on_attach = function(client, bufnr)
+             local ts_utils = require("nvim-lsp-ts-utils")
+             ts_utils.setup {
+                 debug = false,
+                 disable_commands = false,
+                 enable_import_on_completion = false,
+                 import_on_completion_timeout = 5000,
+                 -- eslint
+                 eslint_enable_code_actions = false,
+                 eslint_bin = "eslint_d",
+                 -- eslint_args = {"-f", "json", "--stdin", "--stdin-filename", "$FILENAME"},
+                 eslint_enable_disable_comments = true,
+                 -- eslint diagnostics
+                 eslint_enable_diagnostics = false,
+                 eslint_diagnostics_debounce = 250,
+                 -- formatting
+                 enable_formatting = true,
+                 formatter = "prettierd",
+                 formatter_args = {"--stdin-filepath", "$FILENAME"},
+                 format_on_save = true,
+                 no_save_after_format = false,
+                 -- parentheses completion
+                 complete_parens = false,
+                 signature_help_in_parens = true,
+                 -- update imports on file move
+                 update_imports_on_move = true,
+                 require_confirmation_on_move = false,
+             }
+             client.resolved_capabilities.document_formatting = false
+             -- define an alias
+             vim.cmd("command -buffer Formatting lua vim.lsp.buf.formatting()")
+             vim.cmd("command -buffer FormattingSync lua vim.lsp.buf.formatting_sync()")
+             -- format on save
+             vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+             ts_utils.setup_client(client)
+         end
+    }
    }
  }
 )
@@ -75,4 +117,5 @@ set nohlsearch
 
 require('keys')
 require('telescope-config')
+require('linters-config')
 vim.cmd('source ~/.config/nvim/vim/vimkeys.vim')
