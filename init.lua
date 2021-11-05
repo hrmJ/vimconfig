94abc954266166bae20a985dacb93077c82b3677
Juho Härme <juho.harme@gmail.com>
2021-11-05 19:04:55 +0200

    add basic visuals

--- init.lua
+++ init.lua
@@ -17,13 +17,30 @@ vim.api.nvim_exec(
 local use = require('packer').use
 
 require('packer').startup(function()
-        use 'wbthomason/packer.nvim'
-	use 'neovim/nvim-lspconfig'
-	use {'ray-x/navigator.lua', requires = {'ray-x/guihua.lua', run = 'cd lua/fzy && make'}}
-	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-	use {'ms-jpq/coq_nvim', branch= 'coq', run=':COQdeps'}
-	use {'ms-jpq/coq.thirdparty', branch= '3p'}
+  use 'wbthomason/packer.nvim'
+  use 'neovim/nvim-lspconfig'
+  use {'tomasiser/vim-code-dark', config ='vim.cmd[[colorscheme codedark]]'}
+  use {'ray-x/navigator.lua', requires = {'ray-x/guihua.lua', run = 'cd lua/fzy && make'}}
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  use {'ms-jpq/coq_nvim', branch= 'coq', run=':COQdeps'}
+  use {'ms-jpq/coq.thirdparty', branch= '3p'}
 end)
 
 
 require'navigator'.setup()
+
+vim.o.termguicolors = true
+vim.opt.nu = true
+vim.opt.rnu = true
+vim.opt.wrap = false
+vim.opt.expandtab = true
+vim.opt.hidden = true
+
+vim.cmd [[
+set autoindent
+set expandtab
+set shiftwidth=2
+set smartindent
+set softtabstop=2
+set tabstop=2
+]]
