10028734e67b7dbda2efe08587d88b95802c3b50
Juho Härme <juho.harme@gmail.com>
2021-11-08 20:33:08 +0200

    use lsp_installer for the truly minimal setup

--- init.lua
+++ init.lua
@@ -54,17 +54,33 @@ require('packer').startup(function()
 
 end)
 
-require'lspconfig'.tsserver.setup{
 
-  on_attach = function()
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-    -- Enable completion triggered by <c-x><c-o>
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+local lsp_installer_servers = require'nvim-lsp-installer.servers'
+
+local servers = {'tsserver'}
+
+for _, lsp in ipairs(servers) do
+  local server_available, requested_server = lsp_installer_servers.get_server(lsp)
+  if server_available then
+      requested_server:on_ready(function ()
+          requested_server:setup{
+            on_attach = function()
+              local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+              local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+              -- Enable completion triggered by <c-x><c-o>
+              buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+            end
+            }
+      end)
+      if not requested_server:is_installed() then
+          -- Queue the server to be installed
+          requested_server:install()
+      end
   end
+end
 
-}
+
 
 vim.o.termguicolors = true
 vim.opt.nu = true
