8b8426470de139083b5d5836ca03d59d8817167c
Juho Härme <juho.harme@knowit.fi>
2021-11-17 21:25:40 +0200

    add motion plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -38,6 +38,9 @@ return require('packer').startup(function(use)
 
   use 'andys8/vscode-jest-snippets'
 
+  use 'ggandor/lightspeed.nvim'
+  use 'tpope/vim-repeat'
+
 
  if packer_bootstrap then
     require('packer').sync()
