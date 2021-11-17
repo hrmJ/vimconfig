be62029cf2599eb51d254c9b0af6c10b179976cd
Juho Härme <juho.harme@knowit.fi>
2021-11-17 13:31:50 +0200

    separate plugins to file

--- init.lua
+++ init.lua
@@ -1,17 +1,2 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
-
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-
-  use "neovim/nvim-lspconfig"
-  use "jose-elias-alvarez/null-ls.nvim"
-  use "jose-elias-alvarez/nvim-lsp-ts-utils"
-
- if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+require('plugins')
+require('my-lsp-setup')
