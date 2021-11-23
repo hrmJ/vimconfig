8ab2b7e5c4f679a2e2666e24ae641eef89ea3968
Juho Härme <juho.harme@knowit.fi>
2021-11-23 14:00:56 +0200

    add note about stylua releases

--- lua/lsp/null-lsp-setup.lua
+++ lua/lsp/null-lsp-setup.lua
@@ -1,14 +1,14 @@
-local lspconfig = require("lspconfig")
-local lspUtils = require('lsp.lsp-utils')
+local lspconfig = require 'lspconfig'
+local lspUtils = require 'lsp.lsp-utils'
 
-require("null-ls").config({
-    sources = {
-        require("null-ls").builtins.formatting.prettierd,
-        require("null-ls").builtins.formatting.stylua,
-        require("null-ls").builtins.completion.spell,
-    },
-})
+require('null-ls').config {
+  sources = {
+    require('null-ls').builtins.formatting.prettierd,
+    require('null-ls').builtins.formatting.stylua,
+    require('null-ls').builtins.completion.spell,
+  },
+}
 
+lspconfig['null-ls'].setup { on_attach = lspUtils.on_attach }
 
-lspconfig["null-ls"].setup({ on_attach = lspUtils.on_attach })
-
+-- https://github.com/JohnnyMorganz/StyLua/releases
