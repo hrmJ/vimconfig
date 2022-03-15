e43e161e7e43a56a953d8f6f66d58483eff7ba4d
Juho Härme <juho.harme@knowit.fi>
2022-03-15 14:59:40 +0200

    clean up keys.lua

--- lua/keys.lua
+++ lua/keys.lua
@@ -23,7 +23,6 @@ key('n', '<leader>rr', ':Telescope lsp_references<CR>', { noremap = true, silent
 key('n', '<leader>tl', ':Telescope<CR>', { noremap = true, silent = true })
 key('n', '<C-c>', ':bd<CR>', { noremap = true, silent = true })
 
---key('n', '<leader>dd', ':Telescope lsp_implementations theme=cursor<CR>', { noremap = true, silent = true })
 --
 key('n', '<leader>o', ':Telescope treesitter<CR>', { noremap = true, silent = true })
 
@@ -31,41 +30,24 @@ key('n', '<c-p>', ':Telescope find_files<CR>', { noremap = true, silent = true }
 
 key('n', '<leader>r', ':Telescope git_branches<CR>', { noremap = true, silent = true })
 key('n', '<leader>f', ':Telescope git_files<CR>', { noremap = true, silent = true })
--- key('n', '<leader>f', ':GFiles<CR>', { noremap = true, silent = true })
 
 key('n', '<leader>G', ':Telescope grep_string search="" <CR>', { noremap = true, silent = true })
 key('n', '<leader><C-G>', ':Rg<CR>', { noremap = true, silent = true })
 
---key('n', '<leader>b', ':Buffers<CR>', { noremap = true, silent = true })
 key('n', '<leader>b', ':Telescope buffers<CR>', { noremap = true, silent = true })
+key('n', '<leader>tr', ':Telescope resume<CR>', { noremap = true, silent = true })
 
 key('n', '<space>', '/', { noremap = true, silent = true })
 
 key('n', '<leader>l', ":lua require('telescope.builtin').current_buffer_fuzzy_find({sorting_strategy = 'ascending'})<CR>", { noremap = true, silent = true })
---key('n', '<C-l>', ":BLines<CR>", { noremap = true, silent = true })
 key('n', '<C-l>', ':Lines<CR>', { noremap = true, silent = true })
 
 key('i', ',,', '<c-o>a', { noremap = true, silent = true })
 
--- key('n', '<C-l>', ":lua require('telescope.builtin').current_buffer_fuzzy_find({previewer=false, sorting_strategy = 'ascending'})<CR>", { noremap = true, silent = true })
-
--- key('n', '<leader>G', ':Rg<CR>', { noremap = true, silent = true })
-
--- key('n', '<C-p>', ':Telescope find_files<CR>', { noremap = true, silent = true })
--- key('i', '<C-l>', '<Esc>l:Telescope neoclip theme=cursor<CR>', { noremap = true, silent = true })
--- key('n', '<M-l><M-l>', ":lua require('telescope.builtin').live_grep({grep_open_files=true})<CR>", { noremap = true, silent = true })
--- key('n', '<leader><leader>f', ':HopChar1 <CR>', { noremap = true, silent = true })
--- key('n', '<leader><leader>l', ':HopLine <CR>', { noremap = true, silent = true })
--- key('n', 's', ':HopPattern <CR>', { noremap = true, silent = true })
--- key('n', '<Leader><space>', ':Format <CR>', { noremap = true, silent = true })
--- key('i', ',,', '<c-o>a', { noremap = true, silent = true })
 key('n', 't<C-l>', ':UltestLast<CR>', { noremap = true, silent = true })
 key('n', 't<C-n>', ':UltestNearest<CR>', { noremap = true, silent = true })
 key('n', 't<C-f>', ':Ultest<CR>', { noremap = true, silent = true })
 key('n', 't<C-o>', ':UltestOutput<CR>', { noremap = true, silent = true })
--- key('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
--- key('n', '-', ':NvimTreeFindFileToggle<CR>', { noremap = true, silent = true })
--- key('n', '<C-c>', ':bd<CR>', { noremap = true, silent = true })
 key('v', '<leader>s', ':Telescope grep_string<CR>', { noremap = true, silent = true })
 key('v', '<leader>w', ":lua require('telescope.builtin').grep_string({disable_coordinates=true, word_match = '-w'})<CR>", { noremap = true, silent = true })
 vim.cmd [[nnoremap <silent> <Leader>e <Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>]]
