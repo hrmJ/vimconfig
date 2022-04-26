b80755ac9d6bfdb56c14eacf0c82fdbe1cc0c763
Juho Härme <juho.harme@knowit.fi>
2022-04-26 15:17:40 +0300

    use global statusline

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -1,4 +1,21 @@
-require('lualine').setup()
+require('lualine').setup({
+  options = {
+    globalstatus = true
+  },
+    sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {
+          {
+            'filename',
+            path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+          }
+    },
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+})
 -- require('staline').setup {
 --
 --   sections = {
