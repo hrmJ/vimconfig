22c468fc28a3655c09c0eb57609fa523553aaf6c
Juho Härme <juho.harme@knowit.fi>
2022-10-18 16:55:54 +0300

    basic plugin structure with colors

--- lua/packer-config.lua
+++ lua/packer-config.lua
@@ -1,5 +1,6 @@
 local m = {}
 function m.config(use)
     require 'plugins.git'(use)
+    require 'plugins.colors'(use)
 end
 return m
---
+++ lua/plugins/colors.lua
@@ -0,0 +1,11 @@
+return function(use) 
+	use {
+		"catppuccin/nvim",
+		as = "catppuccin",
+		config = function()
+			vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+			require("catppuccin").setup()
+			vim.api.nvim_command "colorscheme catppuccin"
+		end
+	}
+end
