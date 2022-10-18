5866c02a646db59b0e3884c0ba73b64dfaf5c1f6
Juho Härme <juho.harme@knowit.fi>
2022-10-18 17:03:50 +0300

    add basic plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,9 +1,10 @@
+local plugins = require('packer-config')
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup({
     function(use)
 	use 'wbthomason/packer.nvim'
-        --plugins.config(use)
+        plugins.config(use)
 
         if packer_bootstrap then require('packer').sync() end
     end,
