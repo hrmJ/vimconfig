7822d7fbf4fa97f25cb58393da8a4bf86bf5d17f
Juho Härme <juho.harme@knowit.fi>
2022-10-18 20:00:31 +0300

    add telescope conf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,43 @@
 return function(use) 
+
+
 	use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+	  'nvim-telescope/telescope.nvim', 
+	  tag = '0.1.0',
+	  as = 'telescope',
+	  requires = { {'nvim-lua/plenary.nvim'} },
+	  config = function()
+
+		local actions = require "telescope.actions"
+		require("telescope").setup {
+		  pickers = {
+		    buffers = {
+		      sort_lastused = true,
+		      mappings = {
+			i = {
+			  ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+			}
+		      }
+		    }
+		  },
+		  defaults = {
+		    mappings = {
+		      n = {
+			  ["<esc>"] = false,
+			  ["<C-c>"] = actions.close,
+		      },
+		      i = {
+			  ["<esc>"] = false,
+		      },
+		    },
+		  },
+		  extensions = {
+		  },
+		}
+
+	  end
 	}
+
+
+
 end
