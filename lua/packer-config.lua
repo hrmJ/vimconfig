058c7d414fef4d852b10b2a5e6f35b11bbca2b00
Juho Härme <juho.harme@knowit.fi>
2022-10-18 18:07:16 +0300

    add telescope

--- lua/packer-config.lua
+++ lua/packer-config.lua
@@ -2,5 +2,6 @@ local m = {}
 function m.config(use)
     require 'plugins.git'(use)
     require 'plugins.colors'(use)
+    require 'plugins.telescope'(use)
 end
 return m
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,6 @@
+return function(use) 
+	use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+	}
+end
