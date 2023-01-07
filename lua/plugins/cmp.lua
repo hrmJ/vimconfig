88448e7b055b884ea0af26beda9e38fc3a711838
Juho <juho.harme@gmail.com>
2023-01-07 12:01:52 +0200

    add cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,73 @@
+return {
+  {
+
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/vim-vsnip',
+    'hrsh7th/cmp-nvim-lsp-document-symbol',
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    config = function()
+      local cmp = require 'cmp'
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            vim.fn['vsnip#anonymous'](args.body)
+          end,
+        },
+        mapping = {
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-b>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+          ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' },
+        },
+
+        experimental = {
+          native_menu = false,
+          ghost_text = true,
+        },
+      }
+
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'fuzzy_buffer' },
+        },
+      })
+
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' },
+        }, {
+          { name = 'cmdline', keyword_pattern = [=[[^[:blank:]\!]*]=] },
+        }),
+      })
+
+      cmp.setup.filetype('gitcommit', {
+        sources = cmp.config.sources({
+          { name = 'cmp_git' },
+        }, {
+          { name = 'buffer' },
+        }),
+      })
+
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done { map_char = { tex = '' } })
+    end,
+  },
+}
--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -3,6 +3,14 @@ local key = vim.api.nvim_set_keymap
 key('n', 's', ':Pounce<CR>', { noremap = true, silent = true })
 
 return {
+
+  {
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup {}
+    end,
+  },
+
   {
     'rlane/pounce.nvim',
     config = function()
